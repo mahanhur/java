21b1e2f6a13e951eb2930fd18c63d6b6c19cf39e
mahanhur <124110207+mahanhur@users.noreply.github.com>
2023-02-23 17:03:51 +0900

    Update P68.java
    update test2

--- day02/src/variable/P68.java
+++ day02/src/variable/P68.java
@@ -11,9 +11,6 @@ public class P68 {
 		int a = 10;
 		System.out.println(s2 + 10);
 
-		int n1 = 10;
-		String n2 = String.valueOf(n1);
-
 		Random r = new Random();
 		int rndNum = r.nextInt(10)+1;
 		System.out.println(rndNum);
