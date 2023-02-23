4dbf5f03ea8b49afa0583f36774204be25a10b06
mahanhur <allowbasmh@gmail.com>
2023-02-23 16:37:18 +0900

    test2

--- day02/src/day02/P55.java
+++ day02/src/day02/P55.java
@@ -5,10 +5,16 @@ public class P55 {
 	public static void main(String[] args) {
 		int a = 2000000000;
 		int b = 2000000000;
-		long c = a;
-		long d = b;
-		System.out.println(c+d);
+//		long c = a;
+//		long d = b;
+//		
+		long result = (long)a + (long)b;
+		System.out.println(result);
+		
+//		System.out.println(c+d);
 
+		
+		
 	}
 
 }
