b724b0b45bb3d96494ade836ba6aa1de6c748b1b
mahanhur <allowbasmh@gmail.com>
2023-02-24 09:38:14 +0900

    2023 02 23 assignment

--- day02/src/variable/P69.java
+++ day02/src/variable/P69.java
@@ -3,33 +3,39 @@ package variable;
 public class P69 {
 
 	public static void main(String[] args) {
-		String str = "abcdef";
-		str = str.toUpperCase();
-		System.out.println(str);
+//		String str = "abcdef";
+//		str = str.toUpperCase();
+//		System.out.println(str);
+//		
+//		String str2 = str.replace("A", "Z");
+//		System.out.println(str2);
+//		
+//		int lng = str.length();
+//		System.out.println(lng);
+//		
+//		int t = str.indexOf("B");
+//		System.out.println(t);
+//		
+//		char str3 = str.charAt(0);
+//		System.out.println(str3);
+//		
+//		String str4 = str.substring(1,3);
+//		System.out.println(str4);
 		
-		String str2 = str.replace("A", "Z");
-		System.out.println(str2);
+		String email = "hello2665@navertt.com";
 		
-		int lng = str.length();
+		int at = email.indexOf("@");
+		int lng = email.length();
+		System.out.println(at);
 		System.out.println(lng);
+				
+		String id = email.substring(0,at);
+		String domain = email.substring((at+1), lng);
 		
-		int t = str.indexOf("B");
-		System.out.println(t);
-		
-		char str3 = str.charAt(0);
-		System.out.println(str3);
-		
-		String str4 = str.substring(1,3);
-		System.out.println(str4);
-		
-		String email = "jmlee@tonesol.com";
-		
-//		String id = str.substring(0, 6);
-//		String domain = str.substring(0, 6);
-		
-//		System.out.printf("%s , %s", id, domain);
+		System.out.printf("%s , %s", id, domain);
 		
 		
 	}
 
 }
+
