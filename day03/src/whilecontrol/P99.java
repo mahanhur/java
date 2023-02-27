e4093a192eb07010f5b2e139f2305ea5802286ce
mahanhur <allowbasmh@gmail.com>
2023-02-27 09:34:53 +0900

    add eclipse test

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -5,6 +5,8 @@ public class P99 {
 	public static void main(String[] args) {
 		int ttt = 200;
 		int t = 100;
+		int eclipse = 200;
+		
 		for(int i=2;i<10;i++) {
 			System.out.printf("%d 단 \n",i);
 			for(int j=1;j<10;j++) {
