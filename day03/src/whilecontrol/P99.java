01bdf123f9baabe69d3a78e26c8859d4f8e97e42
mahanhur <allowbasmh@gmail.com>
2023-02-27 09:32:40 +0900

    ttest

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -3,6 +3,8 @@ package whilecontrol;
 public class P99 {
 
 	public static void main(String[] args) {
+		int ttt = 200;
+		int t = 100;
 		for(int i=2;i<10;i++) {
 			System.out.printf("%d 단 \n",i);
 			for(int j=1;j<10;j++) {
